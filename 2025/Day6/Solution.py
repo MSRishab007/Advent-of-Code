6b2e021080aef5747b82082d00da064702c94de6
MSRishab007 <s.rishab0807@gmail.com>
2025-12-06 16:09:25 +0530

    Refactor Day 6 Solution

--- 2025/Day6/Solution.py
+++ 2025/Day6/Solution.py
@@ -17,39 +17,34 @@ for i in zip(data[0],data[1],data[2],data[3],data[4]):
         answer+=a*b*c*d
 print("First Answer:",answer)
 
-answer=0
+answer = 0
 with open("2025\Day6\input.txt", "r") as file:
-    first_line = file.readline().strip("\n")[::-1]
-    second_line = file.readline().strip("\n")[::-1]
-    third_line = file.readline().strip("\n")[::-1]
-    fourth_line = file.readline().strip("\n")[::-1]
-    operators = [i for i in file.readline().strip().split(" ") if i!=""][::-1]
-    operator_index=0
-    nums=[]
-    for i in zip(first_line, second_line, third_line, fourth_line):
-        a,b,c,d=i
-        if a==b==c==d==" ":
-            if operators[operator_index]=="+":
-                answer+=sum(nums)
-            elif operators[operator_index]=="*":
-                prod=1
-                for j in nums:
-                    prod*=j
-                answer+=prod
-            # print("Nums:",nums)
-            nums=[]
-            operator_index+=1
+    lines = [file.readline().strip("\n")[::-1] for _ in range(4)]
+    operators = [i for i in file.readline().strip().split(" ") if i != ""][::-1]
+    
+    nums = []
+    operator_index = 0
+    
+    for chars in zip(*lines):
+        if all(c == " " for c in chars):
+            if nums:
+                result = sum(nums) if operators[operator_index] == "+" else 1
+                if operators[operator_index] == "*":
+                    for n in nums:
+                        result *= n
+                answer += result
+                nums = []
+                operator_index += 1
         else:
-            x=a+b+c+d
-            x=x.strip()
+            x = "".join(chars).strip()
             if x.isdigit():
-                # print(x)
                 nums.append(int(x))
-if operators[operator_index]=="+":
-    answer+=sum(nums)
-elif operators[operator_index]=="*":
-    prod=1
-    for j in nums:
-        prod*=j
-    answer+=prod
-print("Second Answer:",answer)
+    
+    if nums:
+        result = sum(nums) if operators[operator_index] == "+" else 1
+        if operators[operator_index] == "*":
+            for n in nums:
+                result *= n
+        answer += result
+
+print("Second Answer:", answer)
