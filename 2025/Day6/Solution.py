1f2a05cc11af3c57ffc8992a9994b51dcf069244
MSRishab007 <s.rishab0807@gmail.com>
2025-12-06 16:14:49 +0530

    Refactor Day 6 Solution with functions

--- 2025/Day6/Solution.py
+++ 2025/Day6/Solution.py
@@ -1,50 +1,48 @@
-data=[]
-with open("2025\Day6\input.txt", "r") as file:
-    for i in range (0,4):
-        x = file.readline().strip().split(" ")
-        x=[int(i) for i in x if i!=""]
-        data.append(x)
-    x = file.readline().strip().split(" ")
-    x=[i for i in x if i!=""]
-    data.append(x)
-answer=0
+from pathlib import Path
 
-for i in zip(data[0],data[1],data[2],data[3],data[4]):
-    a,b,c,d,e=i
-    if e=="+":
-        answer+=a+b+c+d
-    elif e=="*":
-        answer+=a*b*c*d
-print("First Answer:",answer)
+def calculate_result(nums, operator):
+    """Calculate result based on operator."""
+    if operator == "+":
+        return sum(nums)
+    elif operator == "*":
+        result = 1
+        for n in nums:
+            result *= n
+        return result
+    return 0
 
-answer = 0
-with open("2025\Day6\input.txt", "r") as file:
-    lines = [file.readline().strip("\n")[::-1] for _ in range(4)]
-    operators = [i for i in file.readline().strip().split(" ") if i != ""][::-1]
+def solve():
+    file_path = Path(__file__).parent / "input.txt"
     
+    with open(file_path, "r") as file:
+        lines = [file.readline().strip() for _ in range(4)]
+        operators = [i for i in file.readline().strip().split() if i]
+    answer1 = sum(
+        calculate_result([int(a), int(b), int(c), int(d)], e)
+        for a, b, c, d, e in zip(lines[0].split(), lines[1].split(), 
+                                  lines[2].split(), lines[3].split(), operators)
+    )
+    print("First Answer:", answer1)
+    
+    # Part 2
+    reversed_lines = [line[::-1] for line in lines]
+    reversed_operators = operators[::-1]
+    answer2 = 0
     nums = []
-    operator_index = 0
+    op_idx = 0
     
-    for chars in zip(*lines):
+    for chars in zip(*reversed_lines):
         if all(c == " " for c in chars):
             if nums:
-                result = sum(nums) if operators[operator_index] == "+" else 1
-                if operators[operator_index] == "*":
-                    for n in nums:
-                        result *= n
-                answer += result
+                answer2 += calculate_result(nums, reversed_operators[op_idx])
                 nums = []
-                operator_index += 1
-        else:
-            x = "".join(chars).strip()
-            if x.isdigit():
-                nums.append(int(x))
+                op_idx += 1
+        elif (x := "".join(chars).strip()).isdigit():
+            nums.append(int(x))
     
     if nums:
-        result = sum(nums) if operators[operator_index] == "+" else 1
-        if operators[operator_index] == "*":
-            for n in nums:
-                result *= n
-        answer += result
+        answer2 += calculate_result(nums, reversed_operators[op_idx])
+    
+    print("Second Answer:", answer2)
 
-print("Second Answer:", answer)
+solve()
