c660a3677d2ea2ab52c911d1b8c7a17696f69221
MSRishab007 <s.rishab0807@gmail.com>
2025-12-07 18:24:00 +0530

    Day 7 Part 2

--- 2025/Day7/Solution.py
+++ 2025/Day7/Solution.py
@@ -1,19 +1,36 @@
-with open("2025/Day7/input.txt", "r") as file:
-    lines=[line.strip() for line in file.readlines()]
-    # print(len(lines),len(lines[0]),len(lines[-1]))
-    answer=0
-    next_lines=set()
-    for i in range(0,len(lines)):
-        x=lines[i]
-        for j,y in enumerate(x):
-            if y=='S':
-                next_lines.add(j)
-            if y=="^" and j in next_lines:
-                next_lines.add(j-1)
-                next_lines.add(j+1)
-                next_lines.remove(j)
-                answer+=1
+from functools import cache
+def parse_input(filename):
+    with open(filename, "r") as file:
+        lines=[list(line.strip() )for line in file.readlines()]
+    return lines
+lines = parse_input("2025/Day7/input.txt")
+answer=0
+next_lines=set()
+for i in range(0,len(lines)):
+    x=lines[i]
+    for j,y in enumerate(x):
+        if y=='S':
+            next_lines.add(j)
+        if y=="^" and j in next_lines:
+            next_lines.add(j-1)
+            next_lines.add(j+1)
+            next_lines.remove(j)
+            answer+=1
 print("First Answer:",answer)
-                
-                
-            
+
+@cache
+def helper(current_line,current_position):
+    if current_line>=len(lines):
+        return 1
+    if lines[current_line][current_position]==".":
+        return helper(current_line+2,current_position)
+    elif lines[current_line][current_position]=="^":
+        return helper(current_line+2,current_position-1)+helper(current_line+2,current_position+1)
+
+s_postition=0
+for i in lines[0]:
+    if i=="S":
+        break
+    s_postition+=1
+answer=helper(2,s_postition)
+print("Second Answer:",answer)
